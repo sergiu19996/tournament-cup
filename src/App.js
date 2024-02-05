7c97c792574febaf90bbf055439cd70bc73da7bd
sergiu porime <126587603+sergiu19996@users.noreply.github.com>
2024-02-05 16:19:50 +0000

    adding app.module.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+import NavBar from './components/NavBar';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> hello, world
-      </h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
