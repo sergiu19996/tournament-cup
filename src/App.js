cf818fe585a0c8519486ec23137f752f26645260
sergiu porime <126587603+sergiu19996@users.noreply.github.com>
2024-02-12 19:48:40 +0000

    adding json iteam

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import NavBar from './components/NavBar.js';
 import styles from './App.module.css';
 import  Container  from 'react-bootstrap/Container';
-import {  Route, Switch }  from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
+
 
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
-      <Container>
+      <Container className={styles.Main}>
         <Switch>
-          <Route exact path='/' render={() =>  <h1> League News</h1>} ></Route>
-          <Route exact path='/Signin' render={() =>  <h1>Sign in</h1>} ></Route>
+          <Route exact path="/" render={() =>  <h1> League News</h1>} ></Route>
+          <Route exact path="/Signin" render={() =>  <h1>Sign in</h1>} ></Route>
        </Switch>
       </Container>
     </div>
